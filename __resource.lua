be80acc8a70cdb641ed13606e685030e326efbab
riri1345678 <93609940+riri1345678@users.noreply.github.com>
2021-12-02 17:45:19 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'dlctext.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'carvariations.meta',
+}
+
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+client_scripts 'vehicle_names.lua'
